d68e51ba273c62a7e0c000a1e9ea2d7d0e0100ca
Mieszko Guliński <mieszko_gulinski@wp.pl>
2026-02-06 12:02:52 +0100

    read commands for dir/file in backup

--- cmd/backup/main.go
+++ cmd/backup/main.go
@@ -4,23 +4,51 @@ import (
 	"datalogger/backup"
 	"flag"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	diagnostics := flag.Bool("diagnostics", false, "Write progress information?")
+	inputFile := flag.String("input-file", "", "Path to the database file to be backed up")
+	outputFile := flag.String("output-file", "", "Path to the backup file to be created")
+	dir := flag.String("dir", "", "Directory where backups are stored")
 	flag.Parse()
 
-	err := backup.SetupBackupFile("backup.sqlite", *diagnostics)
+	inputPath := *inputFile
+	if inputPath == "" {
+		inputPath = "db.sqlite"
+	}
+
+	if *outputFile != "" && *dir != "" {
+		log.Fatal("Cannot use --output-file and --dir together")
+	}
+
+	outputPath := ""
+	if *outputFile != "" {
+		outputPath = *outputFile
+	} else if *dir != "" {
+		// Create directory if it doesn't exist
+		err := os.MkdirAll(*dir, 0755)
+		if err != nil {
+			log.Fatalf("Failed to create directory %s: %v", *dir, err)
+		}
+		outputPath = filepath.Join(*dir, "backup.sqlite")
+	} else {
+		outputPath = "backup.sqlite"
+	}
+
+	err := backup.SetupBackupFile(outputPath, *diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = backup.RunBackup("db.sqlite", "backup.sqlite", *diagnostics)
+	err = backup.RunBackup(inputPath, outputPath, *diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = backup.RunIntegrityCheck("backup.sqlite", *diagnostics)
+	err = backup.RunIntegrityCheck(outputPath, *diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
