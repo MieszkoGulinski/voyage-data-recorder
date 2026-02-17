0c6df0ac5c07f155d1af673235a387b72f69cb2e
Mieszko Guliński <mieszko_gulinski@wp.pl>
2026-02-17 20:17:49 +0100

    missing nil check

--- writer/summarizeAndSave.go
+++ writer/summarizeAndSave.go
@@ -13,8 +13,10 @@ func summarizeAndSaveSensors(buffersSet *channels.BuffersSet, db *gorm.DB, diagn
 	// ... more types ...
 
 	err := db.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Create(&weather).Error; err != nil {
-			return err
+		if weather != nil {
+			if err := tx.Create(weather).Error; err != nil {
+				return err
+			}
 		}
 		// ... more types ...
 		return nil
